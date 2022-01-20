74d66f234157d00e86e89decb0050da6e7c90404
Kevin Lin <klin2358@gmail.com>
2022-01-20 17:08:43 -0500

    fixed code and is completed

--- two-sum.js
+++ two-sum.js
@@ -1,12 +1,18 @@
-var arr = [];
-var twoSum = function(nums, target) {
+function twoSum(nums, target) {
+    var arr = [];
     for(i = 0; i < nums.length; i++){
-        for(j = 1; j < nums.length-1; j++){
+        for(j = i + 1; j < nums.length; j++){
             if(nums[i] + nums[j] == target){
                 arr.push(i);
                 arr.push(j);
             }
+            // console.log(nums[i], nums[j])
+            // arr.push(nums[i]+nums[j])
         }
     }
     return arr;
 };
+
+console.log(twoSum([2,7,11,15], 9));
+console.log(twoSum([3,2,4], 6));
+console.log(twoSum([3,3], 6));
