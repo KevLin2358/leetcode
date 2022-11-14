7ee774b6a97d4df5e8c56f22db29ac234402f323
KevLin2359 <klin2358@gmail.com>
2022-11-14 18:26:03 -0500

    structy anagrams

---
+++ structy/array_and_strings/anagrams.js
@@ -0,0 +1,22 @@
+const anagrams = (s1, s2) => {
+  // todo
+  const count = {};
+  for(let char of s1){
+    if(!(char in count)) count[char] = 0;
+    count[char] += 1;
+  }
+  
+  for(let char of s2){
+    if(!(char in count)){
+      return false;
+    }else{
+      count[char] -= 1;
+    }
+  }
+  
+  for(let char in count){
+    if(count[char] !== 0)return false;
+  }
+  
+  return true;
+};
