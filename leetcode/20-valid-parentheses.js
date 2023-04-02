e46feb319710c428e70dae193b76882dee261be9
KevLin2358 <klin2358@gmail.com>
2023-04-02 19:28:17 -0400

    refactored valid parentheses

--- leetcode/20-valid-parentheses.js
+++ leetcode/20-valid-parentheses.js
@@ -1,34 +1,34 @@
-var isValid = function (s) {
-  let leftSide = [];
+// var isValid = function (s) {
+//   let leftSide = [];
 
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
-      leftSide.push(s[i]);
-    } else if (
-      s[i] === ")" &&
-      leftSide.length !== 0 &&
-      leftSide[leftSide.length - 1] === "("
-    ) {
-      leftSide.pop();
-    } else if (
-      s[i] === "]" &&
-      leftSide.length !== 0 &&
-      leftSide[leftSide.length - 1] === "["
-    ) {
-      leftSide.pop();
-    } else if (
-      s[i] === "}" &&
-      leftSide.length !== 0 &&
-      leftSide[leftSide.length - 1] === "{"
-    ) {
-      leftSide.pop();
-    } else return false;
-  }
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+//       leftSide.push(s[i]);
+//     } else if (
+//       s[i] === ")" &&
+//       leftSide.length !== 0 &&
+//       leftSide[leftSide.length - 1] === "("
+//     ) {
+//       leftSide.pop();
+//     } else if (
+//       s[i] === "]" &&
+//       leftSide.length !== 0 &&
+//       leftSide[leftSide.length - 1] === "["
+//     ) {
+//       leftSide.pop();
+//     } else if (
+//       s[i] === "}" &&
+//       leftSide.length !== 0 &&
+//       leftSide[leftSide.length - 1] === "{"
+//     ) {
+//       leftSide.pop();
+//     } else return false;
+//   }
 
-  if (leftSide.length > 0) return false;
+//   if (leftSide.length > 0) return false;
 
-  return true;
-};
+//   return true;
+// };
 
 // let stack = [];
 // for(let i = 0; i < s.length; i++){
@@ -41,3 +41,24 @@ var isValid = function (s) {
 
 // const result = stack.length > 0 ? false : true;
 // return result;
+
+var isValid = function(s) {
+  let obj = {
+      ']' : '[',
+      '}' : '{',
+      ')' : '(',
+  }
+
+  let stack = []
+  let i = 0;
+
+  while (i < s.length) {
+      let ele = s[i]
+      if (ele === '(' || ele ==='{' || ele === '[') stack.push(ele)
+      else if (obj[ele] === stack[stack.length - 1]) stack.pop()
+      else return false
+      i++
+  }
+  if (stack.length > 0) return false
+  return true
+};
