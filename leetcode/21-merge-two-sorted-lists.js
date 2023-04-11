cb270b150af387ec49e4574873e64476f916c65b
KevLin2359 <klin2358@gmail.com>
2023-04-11 16:45:09 -0400

    21 merge two sorted list

--- leetcode/21-merge-two-sorted-lists.js
+++ leetcode/21-merge-two-sorted-lists.js
@@ -1,24 +1,46 @@
-var mergeTwoLists = function (list1, list2) {
-  if (list1 === 0 && list2 === 0) return [];
+// var mergeTwoLists = function (list1, list2) {
+//   if (list1 === 0 && list2 === 0) return [];
 
-  let dummy = new ListNode(0, null);
-  let head = dummy;
+//   let dummy = new ListNode(0, null);
+//   let head = dummy;
 
-  let current1 = list1;
-  let current2 = list2;
+//   let current1 = list1;
+//   let current2 = list2;
 
-  while (current1 && current2) {
-    if (current1.val <= current2.val) {
-      dummy.next = current1;
+//   while (current1 && current2) {
+//     if (current1.val <= current2.val) {
+//       dummy.next = current1;
 
-      current1 = current1.next;
-    } else {
-      dummy.next = current2;
-      current2 = current2.next;
+//       current1 = current1.next;
+//     } else {
+//       dummy.next = current2;
+//       current2 = current2.next;
+//     }
+//     dummy = dummy.next;
+//   }
+//   if (current1 !== null) dummy.next = current1;
+//   if (current2 !== null) dummy.next = current2;
+//   return head.next;
+// };
+
+var mergeTwoLists = function(list1, list2) {
+    let list = new ListNode(0)
+    let head = list
+
+    while (list1 && list2) {
+        if(list1.val <= list2.val) {
+            list.next = list1
+            list1 = list1.next
+        } else {
+            list.next = list2
+            list2 = list2.next
+        }
+
+        list = list.next
     }
-    dummy = dummy.next;
-  }
-  if (current1 !== null) dummy.next = current1;
-  if (current2 !== null) dummy.next = current2;
-  return head.next;
+
+    if (list1) list.next = list1
+    if (list2) list.next = list2
+
+    return head.next
 };
