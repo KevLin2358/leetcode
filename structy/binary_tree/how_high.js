0e3f15b86b620ed438a32484e745202117866471
KevLin2358 <klin2358@gmail.com>
2023-04-23 04:57:58 -0400

    how high

--- structy/binary_tree/how_high.js
+++ structy/binary_tree/how_high.js
@@ -1,5 +1,43 @@
-const howHigh = (node) => {
-  // todo
-  if (node === null) return -1;
-  return Math.max(howHigh(node.left), howHigh(node.right)) + 1;
+// iterative
+
+const howHighIterative = (node) => {
+  // base case
+  if (!node) return -1
+  if (!node.left && !node.right) return 0
+  
+  let count = 0
+  let stack = [ [node] ]
+  let flag = true
+  
+  while (flag) {
+    let curr = stack.pop()    
+    let arr = []
+    
+    for (let i = 0; i < curr.length; i++) {
+      let currNode = curr[i]
+      if (currNode.left) arr.push(currNode.left)
+      if (currNode.right) arr.push(currNode.right)
+    }
+    
+    flag = arr.some((currNode) => {
+      if (!currNode.left && !currNode.right) return false
+      return true
+    })
+  
+    count++
+    stack.push(arr)
+  }
+  
+  return count
 };
+
+
+const howHigh = (node) => {
+  // base case
+  if (!node) return -1
+
+  const leftSide = howHigh(node.left)
+  const rightSide = howHigh(node.right)
+  
+  return 1 + Math.max(leftSide, rightSide)
+}
