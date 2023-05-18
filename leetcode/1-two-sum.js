f5dc7ffd18fd8a02155cd6cb2189cf85f585bc39
KevLin2358 <klin2358@gmail.com>
2023-05-17 20:19:56 -0400

    revised two sum

--- leetcode/1-two-sum.js
+++ leetcode/1-two-sum.js
@@ -21,3 +21,34 @@ Memory
 Beats
 90.74%
 */
+
+
+var twoSum2 = function(nums, target) {
+    let obj = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        let diff = target - nums[i]
+
+        if (obj[diff] !== undefined) return [i, obj[diff]]
+        else obj[nums[i]] = i
+
+    }
+};
+
+/*
+
+Runtime
+
+    64 ms
+
+Beats 80.13%
+
+
+Memory
+
+    42.6 MB
+
+Beats 56.46%
+
+
+*/
