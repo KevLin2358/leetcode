d7cbb237ba242c1fc4e27959490b382c87d190e5
KevLin2358 <klin2358@gmail.com>
2023-08-23 19:20:55 -0400

    aug 23 tree revisited

--- structy/JS/4_binary_tree/7_tree_path_finder.js
+++ structy/JS/4_binary_tree/7_tree_path_finder.js
@@ -1,51 +1,70 @@
 // depth first
 // time conplexity : O(n^2)
 // space complexity : O(n)
+
 const pathFinder = (root, target) => {
-  // base case
+  // todo
   if (!root) return null
-  if (root.val === target) return [root.val]
+  if (root.val === target) return [ target ]
   
-  // two paths
-  const leftPath = pathFinder(root.left, target)
-  const rightPath = pathFinder(root.right, target)
+  let leftSide = pathFinder(root.left, target)
+  let rightSide = pathFinder(root.right, target)
   
-  // either left, right or not found
-  if(leftPath) return [root.val, ...leftPath]
-  if(rightPath) return [root.val, ...rightPath]
+  if (leftSide) {
+    return [root.val, ...leftSide]
+  }
+  
+  if (rightSide) {
+    return [root.val, ...rightSide]
+  }
+  
   return null
 };
-
-
-// depth first w/ push
-// time conplexity : O(n)
-// space complexity : O(n)
-const pathFinderPush = (root, target) => {
-  const res = pathFinderHelper(root, target)
-  if (!res) return null
-  return res.reverse()
-}
-
-const pathFinderHelper = (root, target) => {
-  // base case
-  if (root === null) return null
-  if (root.val === target) return [ root.val ]
-
-  // two paths
-  const leftPath = pathFinderHelper(root.left, target)
-  const rightPath = pathFinderHelper(root.right, target)
-
-  // left, right will use push
-  if (leftPath) {
-    leftPath.push(root.val)
-    return leftPath
-  }
-
-  if (rightPath) {
-    rightPath.push(root.val)
-    return rightPath
-  }
+// const pathFinder = (root, target) => {
+//   // base case
+//   if (!root) return null
+//   if (root.val === target) return [root.val]
   
-  // nothing return null
-  return null
-}
+//   // two paths
+//   const leftPath = pathFinder(root.left, target)
+//   const rightPath = pathFinder(root.right, target)
+  
+//   // either left, right or not found
+//   if(leftPath) return [root.val, ...leftPath]
+//   if(rightPath) return [root.val, ...rightPath]
+//   return null
+// };
+
+
+// // depth first w/ push
+// // time conplexity : O(n)
+// // space complexity : O(n)
+// const pathFinderPush = (root, target) => {
+//   const res = pathFinderHelper(root, target)
+//   if (!res) return null
+//   return res.reverse()
+// }
+
+// const pathFinderHelper = (root, target) => {
+//   // base case
+//   if (root === null) return null
+//   if (root.val === target) return [ root.val ]
+
+//   // two paths
+//   const leftPath = pathFinderHelper(root.left, target)
+//   const rightPath = pathFinderHelper(root.right, target)
+
+//   // left, right will use push
+//   if (leftPath) {
+//     leftPath.push(root.val)
+//     return leftPath
+//   }
+
+//   if (rightPath) {
+//     rightPath.push(root.val)
+//     return rightPath
+//   }
+  
+//   // nothing return null
+//   return null
+// }
